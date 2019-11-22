8b59c28a3e2bcf5d5748f023b37dc1127cf89ff6
yetao <981599614@qq.com>
2019-11-22 15:02:11 +0800

    no message

---
+++ src/views/component/Tinymce.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+export default class TinymceComponent extends React.Component {
+    render() {
+        return (
+            <div>
+                444433333333
+            </div>
+        );
+    }
+}
+

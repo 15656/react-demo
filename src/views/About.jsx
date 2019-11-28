0e7347f273663e318fdf720e448f5b726d513285
yetao <981599614@qq.com>
2019-11-28 09:26:01 +0800

    no message

--- src/views/About.jsx
+++ src/views/About.jsx
@@ -2,15 +2,30 @@ import React,{Component} from 'react';
 import {HttpGet} from '../api/index'
 
 class About extends Component {
+  componentDidMount(){
+    this.init();
+  }
+  async init(){
+    const data = await HttpGet('member/back/city/list/1',{});
+    let id = null;
+    if(data){
+      id = data.data[1].id;
+    }
+    const data1 = await HttpGet('member/back/city/list/'+id,{});
+    if(data1){
+      console.log(data1)
+    }
+   
+    // .then(res =>{
+    //   if(res.code === 0){
+    //     //js跳转
+    //     // setTimeout(() =>{
+    //     //   this.props.history.push({ pathname : '/redux' ,query : res.data })
+    //     // },5000)
+    //   }
+    // })
+  }
   render() {
-    HttpGet('member/back/city/list/1',{}).then(res =>{
-      if(res.code === 0){
-        //js跳转
-        setTimeout(() =>{
-          this.props.history.push({ pathname : '/redux' ,query : res.data })
-        },5000)
-      }
-    })
     console.log(this.props)
     return (
       <div>
